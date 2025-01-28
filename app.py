42c2631d119729661f26482331ea1f6d0d34c09e
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-01-28 23:11:17 +0600

    practice day1

---
+++ app.py
@@ -0,0 +1,21 @@
+# print("Hello, Ruhul! welcome to python")
+# name = "Ruhul"
+# age = 32
+# print("your name is:", name)
+# print('your age is:', age)
+
+# name = input("what is your name?")
+# print("Hello," + name + "! Welcome to Python.")
+# number1 = int(input("Enter first number:"))
+# number2 = int(input("Enter second number:"))
+# Sum = number1 + number2
+#print("the sum of two Numbers is", Sum)
+# number3 = int(input("Enter third number:"))
+# if number3 % 2 == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+input1 = int(input("Enter rectangle length:"))
+input2 = int(input("Enter rectangle width:"))
+area = input1 * input2
+print("The area of rectangle is", area)
