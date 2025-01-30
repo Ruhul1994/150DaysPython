c1c9a029d528a0bd53266187c7bb4c351d6f2b6e
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-01-30 21:54:10 +0600

    Add project files and configuration for Day03

---
+++ Day03/main.py
@@ -0,0 +1,18 @@
+
+# def multiplies(a,b):
+#     return a * b
+#
+# print(multiplies(5,6))
+
+# def add_numbers (a, b):
+#     return a + b
+# print(add_numbers(5,6))
+
+def my_function(firstname, lastname):
+    fullname = firstname + " " + lastname
+    print("this is my fullname: ", fullname)
+    return fullname
+
+
+my_function("jon", "don")
+
