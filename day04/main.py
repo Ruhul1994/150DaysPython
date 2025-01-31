7f08042efcb8daed8217b1916aab8108a37f4465
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-01-31 23:35:53 +0600

    Add project files and configuration for Day04

---
+++ day04/main.py
@@ -0,0 +1,34 @@
+
+fruits = ["apple", "banana", "cherry","mango"]
+for fruit in fruits:
+    print(fruit)
+
+print(fruits[0])
+print(fruits[1])
+
+fruits[1]="watermelon"
+fruits.insert(3,"pineapple")
+
+fruits.remove("cherry")
+print(fruits)
+fruits.pop(2)
+print(fruits)
+
+i= 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
+
+    fruits.sort()
+print(fruits)
+
+fruits.reverse()
+fruits.append("grape")
+print(fruits)
+new_fruits = fruits.copy()
+print(new_fruits)
+number=[1,2,3,4,5,6,7,8,9,10]
+summation = 0
+for i in number:
+    summation += i
+print(summation)
