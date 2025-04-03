69da2445c5917fb9c30d2d4069f18b1d852d1d39
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-04-04 05:46:06 +0600

    Add initial example script for Day06 with basic operations and input handling

---
+++ Day06/index.py
@@ -0,0 +1,23 @@
+# print('Hello, world!')
+
+# Bank_Account = 1000
+# food = 100
+# clothes = 200
+# car = 300
+# house = 200
+# total = food + clothes + car + house
+# print('Bank Account:', Bank_Account - total)
+
+# first_name = input('Enter your first name: ')
+# last_name = input('Enter your last name: ')
+# full_name = f"{first_name} {last_name}"
+# print('Full name:', full_name)
+
+# Mark =70
+
+# if Mark >= 50:
+#     print('pass')
+# elif Mark >=60:
+#     print('first class')
+# else:
+#     print('fail')
